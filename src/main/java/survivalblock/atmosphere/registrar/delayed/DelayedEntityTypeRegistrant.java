429ca822e8ccd4594ec30b96314f3c642a245e3b
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2026-08-15 21:23:07 -0700

    Unconstrain imports

--- src/main/java/survivalblock/atmosphere/registrar/delayed/DelayedEntityTypeRegistrant.java
+++ src/main/java/survivalblock/atmosphere/registrar/delayed/DelayedEntityTypeRegistrant.java
@@ -25,7 +25,6 @@ package survivalblock.atmosphere.registrar.delayed;
 
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
-//? if >=1.21.2
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.entity.Entity;
