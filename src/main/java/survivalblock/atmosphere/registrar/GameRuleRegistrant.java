964fe27adb93cb0def9e11baaaa77406c6f4d213
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2026-08-17 20:58:22 -0700

    Spotless, fix game rules

--- src/main/java/survivalblock/atmosphere/registrar/GameRuleRegistrant.java
+++ src/main/java/survivalblock/atmosphere/registrar/GameRuleRegistrant.java
@@ -1,5 +1,32 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025-present ekulxam
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package survivalblock.atmosphere.registrar;
 
+//? if <1.21.11 {
+/*import net.fabricmc.fabric.api.gamerule.v1.rule.DoubleRule;
+import net.fabricmc.fabric.api.gamerule.v1.rule.EnumRule;
+*///?}
 import net.minecraft.core.Registry;
 //? if >=1.21.11
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -8,14 +35,14 @@ import net.minecraft.resources.Identifier;
 /*import net.minecraft.world.level.GameRules;
 *///?} else {
 import net.minecraft.world.level.gamerules.GameRule;
-//?}
 import net.minecraft.world.level.gamerules.GameRules;
+//?}
 import survivalblock.atmosphere.registrar.shared.IGameRuleRegistrant;
 
 import java.util.function.Function;
 
 /**
- * A {@link Registrant} for game rules. Note that before 1.21.11, the registry is null.
+ * A {@link Registrant} for game rules. Note that before 1.21.11, the {@link #registry} is null because it doesn't exist.
  */
 @SuppressWarnings("unused")
 //~ if >=1.21.11 'GameRules.Key<?>' -> 'GameRule<?>' {
--- src/main/java/survivalblock/atmosphere/registrar/shared/IGameRuleRegistrant.java
+++ src/main/java/survivalblock/atmosphere/registrar/shared/IGameRuleRegistrant.java
@@ -1,3 +1,26 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025-present ekulxam
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package survivalblock.atmosphere.registrar.shared;
 
 //? if >=1.21.11 {
@@ -9,13 +32,11 @@ import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
 import net.fabricmc.fabric.api.gamerule.v1.rule.DoubleRule;
 import net.fabricmc.fabric.api.gamerule.v1.rule.EnumRule;
 *///?}
-import net.minecraft.core.Registry;
 //? if >=1.21.11 {
 import net.minecraft.core.registries.BuiltInRegistries;
 //?} else {
 /*import net.minecraft.resources.ResourceKey;
  *///?}
-import net.minecraft.resources.Identifier;
 //? if <1.21.11 {
 /*import net.minecraft.world.level.GameRules;
  *///?} else {
@@ -45,33 +66,35 @@ public interface IGameRuleRegistrant extends IRegistrant<GameRule<?>> {
 
     //? if <1.21.11 {
     /*@Override
-    public ResourceKey<GameRules.Key<?>> createKey(String name) {
+    default ResourceKey<GameRules.Key<?>> createKey(String name) {
         throw new UnsupportedOperationException("Not allowed before 1.21.11!");
     }
 
     @Override
-    public <U extends GameRules.Key<?>> U register(String name, U obj) {
+    default <U extends GameRules.Key<?>> U register(String name, U obj) {
         throw new UnsupportedOperationException("Not allowed before 1.21.11!");
     }
 
     @Override
-    public <U extends GameRules.Key<?>> U register(ResourceKey<GameRules.Key<?>> key, U obj) {
+    default <U extends GameRules.Key<?>> U register(ResourceKey<GameRules.Key<?>> key, U obj) {
         throw new UnsupportedOperationException("Not allowed before 1.21.11!");
     }
 
-    public GameRules.Key<GameRules.BooleanValue> registerBoolean(String name, GameRules.Category category, boolean defaultValue) {
+    @SuppressWarnings("unused")
+    default GameRules.Key<GameRules.BooleanValue> registerBoolean(String name, GameRules.Category category, boolean defaultValue) {
         return this.register(name, category, GameRuleFactory.createBooleanRule(defaultValue));
     }
 
-    public GameRules.Key<GameRules.BooleanValue> registerBoolean(String name, CustomGameRuleCategory category, boolean defaultValue) {
+    @SuppressWarnings("unused")
+    default GameRules.Key<GameRules.BooleanValue> registerBoolean(String name, CustomGameRuleCategory category, boolean defaultValue) {
         return this.register(name, category, GameRuleFactory.createBooleanRule(defaultValue));
     }
 
-    public <T extends GameRules.Value<T>> GameRules.Key<T> register(String name, GameRules.Category category, GameRules.Type<T> type) {
+    default <T extends GameRules.Value<T>> GameRules.Key<T> register(String name, GameRules.Category category, GameRules.Type<T> type) {
         return GameRuleRegistry.register(this.id(name).toString(), category, type);
     }
 
-    public <T extends GameRules.Value<T>> GameRules.Key<T> register(String name, CustomGameRuleCategory category, GameRules.Type<T> type) {
+    default <T extends GameRules.Value<T>> GameRules.Key<T> register(String name, CustomGameRuleCategory category, GameRules.Type<T> type) {
         return GameRuleRegistry.register(this.id(name).toString(), category, type);
     }
     *///?}
@@ -99,7 +122,7 @@ public interface IGameRuleRegistrant extends IRegistrant<GameRule<?>> {
     //~}
 
     static <E extends Enum<E>> E getEnum(GameRules gameRules, GameRule</*? <1.21.11 {*/ /*EnumRule<E> *//*?} else {*/ E /*?}*/> enumRule) {
-        //~ if >=1.21.11 'getRule(enumRule).get() -> 'get(enumRule)'
+        //~ if >=1.21.11 'getRule(enumRule).get()' -> 'get(enumRule)'
         return gameRules.get(enumRule);
     }
     //~}
